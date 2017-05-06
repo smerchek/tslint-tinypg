68bf00554a1f39b2135ad267deb087f30011898b
Scott Smerchek <scott.smerchek@gmail.com>
2017-05-06 16:38:27 -0500

    Refactor walk function to use a util method.

--- src/noMissingSqlFileRule.ts
+++ src/noMissingSqlFileRule.ts
@@ -2,6 +2,7 @@ import * as ts from 'typescript'
 import * as Lint from 'tslint'
 import * as globby from 'globby'
 import * as _ from 'lodash'
+import { forEachTinySqlCall } from './util'
 
 export class Rule extends Lint.Rules.AbstractRule {
    public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
@@ -12,36 +13,18 @@ export class Rule extends Lint.Rules.AbstractRule {
 function walk(ctx: Lint.WalkContext<void>): void {
    const tinySqlFiles: string[] = globby.sync(['**/*.sql', '!**/node_modules/**'])
 
-   const cb = (node: ts.Node): void => {
-      if (node && node.kind === ts.SyntaxKind.CallExpression) {
-         const call_expression = node as ts.CallExpression
-         const property_expression = call_expression.expression as ts.PropertyAccessExpression
+   const hook = (sql_call_name, call_expression) => {
+      const tiny_file_path = `${_.trim(sql_call_name, `'`).replace(/\./g, '/')}.sql`
 
-         if (!property_expression || !property_expression.name) {
-            return
-         }
-
-         const property_name = property_expression.name.text
-
-         if (property_name === 'sql' && call_expression.arguments[0].kind === ts.SyntaxKind.StringLiteral) {
-            const sql_call_text = call_expression.arguments[0].getText()
-            const tiny_file_path = `${_.trim(sql_call_text, `'`).replace(/\./g, '/')}.sql`
-
-            if (!_.some(tinySqlFiles, x => _.includes(x, tiny_file_path))) {
-               ctx.addFailure(
-                  call_expression.arguments[0].getStart(ctx.sourceFile),
-                  call_expression.arguments[0].getStart(ctx.sourceFile) + call_expression.arguments[0].getWidth(),
-                  `No SQL file found for ${sql_call_text}.`
-               )
-            }
-
-         }
-
+      if (!_.some(tinySqlFiles, x => _.includes(x, tiny_file_path))) {
+         ctx.addFailure(
+            call_expression.arguments[0].getStart(ctx.sourceFile),
+            call_expression.arguments[0].getStart(ctx.sourceFile) + call_expression.arguments[0].getWidth(),
+            `No SQL file found for [${sql_call_name}].`
+         )
       }
-      // Wondering why return is used below? Refer to "Make use of tail calls"
-      return ts.forEachChild(node, cb) // recurse deeper
    }
 
-   return ts.forEachChild(ctx.sourceFile, cb) // start recursion with children of sourceFile
+   return forEachTinySqlCall(ctx, hook)
 }
 
---
+++ src/util.ts
@@ -0,0 +1,31 @@
+import * as ts from 'typescript'
+import * as Lint from 'tslint'
+import * as _ from 'lodash'
+
+type onSqlCallHook = (sql_call_name: string, call_expression: ts.CallExpression) => void
+
+export function forEachTinySqlCall(ctx: Lint.WalkContext<void>, onSqlCall: onSqlCallHook): void {
+   const cb = (node: ts.Node): void => {
+      if (node && node.kind === ts.SyntaxKind.CallExpression) {
+         const call_expression = node as ts.CallExpression
+         const property_expression = call_expression.expression as ts.PropertyAccessExpression
+
+         if (!property_expression || !property_expression.name) {
+            return
+         }
+
+         const property_name = property_expression.name.text
+
+         if (property_name === 'sql' && call_expression.arguments[0].kind === ts.SyntaxKind.StringLiteral) {
+            const sql_call_name = _.trim(call_expression.arguments[0].getText(), `'"\``)
+
+            onSqlCall(sql_call_name, call_expression)
+         }
+
+      }
+      // Wondering why return is used below? Refer to "Make use of tail calls"
+      return ts.forEachChild(node, cb) // recurse deeper
+   }
+
+   return ts.forEachChild(ctx.sourceFile, cb) // start recursion with children of sourceFile
+}
